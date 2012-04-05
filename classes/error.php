7e0dd0702c48d919766a4a8719cd5b71a86297d2
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-04-05 14:42:34 +0100

    Add error class

---
+++ classes/error.php
@@ -0,0 +1,22 @@
+<?php
+abstract class WFErrorHandler
+{
+	public static function suppressError($errno, $errstr, $errfile, $errline)
+	{
+		switch($errno) {
+			default:
+				if (defined('JCE_REQUEST')) {
+					return true;
+				} else {
+					return false;
+				}
+				break;
+			case E_STRICT:
+				return true;
+				break;
+		}
+	}
+}
+// suppress E_STRICT warnings
+set_error_handler(array('WFErrorHandler', 'suppressError'), E_ALL | E_STRICT);
+?>
