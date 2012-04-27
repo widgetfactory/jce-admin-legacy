10f8ab4312deeec6f494163fc69db3eacaf01d6f
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-04-27 09:29:55 +0100

    Reformat

--- models/cpanel.php
+++ models/cpanel.php
@@ -1,91 +1,87 @@
 <?php
 /**
  * @package   	JCE
- * @copyright 	Copyright © 2009-2011 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2011 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('_JEXEC') or die('RESTRICTED');
 
 // load base model
 require_once(dirname(__FILE__) . DS . 'model.php');
 
-class WFModelCpanel extends WFModel
-{
-    function iconButton($link, $image, $text, $description = '', $disabled = false)
-    {
+class WFModelCpanel extends WFModel {
+
+    function iconButton($link, $image, $text, $description = '', $disabled = false) {
         $lang = JFactory::getLanguage();
-        
+
         if ($disabled) {
             $link = '#';
         }
-        
+
         $description = $description ? $text . '::' . $description : $text;
         ?>
-        <li class="cpanel-icon tooltip ui-corner-all" title="<?php echo $description;?>">
-          <a href="<?php echo $link;?>"><?php echo JHTML::_('image.site', $image, '/components/com_jce/media/img/cpanel/', NULL, NULL, $text);?><?php echo $text;?></a>
+        <li class="cpanel-icon tooltip ui-corner-all" title="<?php echo $description; ?>">
+            <a href="<?php echo $link; ?>"><?php echo JHTML::_('image.site', $image, '/components/com_jce/media/img/cpanel/', NULL, NULL, $text); ?><?php echo $text; ?></a>
         </li>
         <?php
     }
-    
-    function getVersion()
-    {
+
+    function getVersion() {
         // Get Component xml
         $xml = JApplicationHelper::parseXMLInstallFile(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jce' . DS . 'jce.xml');
-        
+
         return $xml['version'];
     }
-    
-    function getLicense()
-    {
+
+    function getLicense() {
         return '<a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html" title="GNU General Public License, version 2" target="_blank">GNU General Public License, version 2</a>';
     }
-    
-    function getFeeds()
-    {
-        $app    = JFactory::getApplication();
+
+    function getFeeds() {
+        $app = JFactory::getApplication();
         $params = JComponentHelper::getParams('com_jce');
-        $limit  = $params->get('feed_limit', 2);
-        
-        $feeds   = array();
+        $limit = $params->get('feed_limit', 2);
+
+        $feeds = array();
         $options = array(
             'rssUrl' => 'http://www.joomlacontenteditor.net/news/feed/rss/latest-news?format=feed',
             'cache_time' => $params->get('feed_cachetime', 86400)
         );
-        
+
         // use this directly instead of JFactory::getXMLParserto avoid the feed data error
         jimport('simplepie.simplepie');
-        
+
         if (!is_writable(JPATH_BASE . DS . 'cache')) {
             $options['cache_time'] = 0;
         }
         $rss = new SimplePie($options['rssUrl'], JPATH_BASE . DS . 'cache', isset($options['cache_time']) ? $options['cache_time'] : 0);
         $rss->force_feed(true);
         $rss->handle_content_type();
-        
+
         if ($rss->init()) {
             $count = $rss->get_item_quantity();
-            
+
             if ($count) {
                 $count = ($count > $limit) ? $limit : $count;
                 for ($i = 0; $i < $count; $i++) {
                     $feed = new StdClass();
                     $item = $rss->get_item($i);
-                    
-                    $feed->link        = $item->get_link();
-                    $feed->title       = $item->get_title();
+
+                    $feed->link = $item->get_link();
+                    $feed->title = $item->get_title();
                     $feed->description = $item->get_description();
-                    
+
                     $feeds[] = $feed;
                 }
             }
         }
-        
+
         return $feeds;
     }
+
 }
 ?>
