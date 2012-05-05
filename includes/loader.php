4619e5a3dbeed428dcb3753fbce2b984a9b19320
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-05-05 13:37:28 +0100

    Formatting

--- includes/loader.php
+++ includes/loader.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   	JCE
  * @copyright 	Copyright © 2009-2011 Ryan Demmer. All rights reserved.
@@ -8,53 +9,53 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
-defined( '_JEXEC') or die('RESTRICTED');
+defined('_JEXEC') or die('RESTRICTED');
 
 abstract class WFLoader {
-	/**
-	 * Loads a file from specified directories.
-	 *
-	 * @param string $name	The class name to look for ( dot notation ).
-	 * @return void
-	 * @copyright Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
-	 */
-	public static function import($path) {
-		
-		static $paths;
 
-		if (!isset($paths)) {
-			$paths = array();
-		}
-		
-		$parts 	= explode('.', $path);
-		$key 	= array_shift($parts);
-		
-		$keyPath = $key ? $key . '.' . implode('.', $parts) : $path;
+    /**
+     * Loads a file from specified directories.
+     *
+     * @param string $name	The class name to look for ( dot notation ).
+     * @return void
+     * @copyright Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+     */
+    public static function import($path) {
 
-		if (!isset($paths[$keyPath]))
-		{
-			// set base as Component root
-			$base 	= JPATH_COMPONENT_ADMINISTRATOR;
-	
-			switch ($key) {
-				case 'editor':
-					$base = JPATH_SITE .DS. 'components' .DS. 'com_jce' .DS. 'editor';
-					break;
-				case 'admin':
-					$base = dirname(dirname(__FILE__));
-					break;
-			}
-				
-			$rs = require_once($base . DS . implode(DS, $parts) . '.php');	
-			
-			$paths[$keyPath] = $rs;
-		}
-		
-		return $paths[$keyPath];
-	}
+        static $paths;
+
+        if (!isset($paths)) {
+            $paths = array();
+        }
+
+        $parts = explode('.', $path);
+        $key = array_shift($parts);
+
+        $keyPath = $key ? $key . '.' . implode('.', $parts) : $path;
+
+        if (!isset($paths[$keyPath])) {
+            // set base as Component root
+            $base = JPATH_COMPONENT_ADMINISTRATOR;
+
+            switch ($key) {
+                case 'editor':
+                    $base = JPATH_SITE . DS . 'components' . DS . 'com_jce' . DS . 'editor';
+                    break;
+                case 'admin':
+                    $base = dirname(dirname(__FILE__));
+                    break;
+            }
+
+            $rs = require_once($base . DS . implode(DS, $parts) . '.php');
+
+            $paths[$keyPath] = $rs;
+        }
+
+        return $paths[$keyPath];
+    }
+
 }
 
 function wfimport($path) {
-	return WFLoader::import($path);
+    return WFLoader::import($path);
 }
