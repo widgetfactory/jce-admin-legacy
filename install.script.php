49fb7af476fcab599ef468969b4744b090402e35
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-05-16 17:07:42 +0100

    Add install.script file for Joomla! 2.5

---
+++ install.script.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @package   	JCE
+ * @copyright 	Copyright (c) 2009-2012 Ryan Demmer. All rights reserved.
+ * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * JCE is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+defined('_JEXEC') or die('RESTRICTED');
+
+class com_jceInstallerScript {
+
+    public function install($parent) {
+        if (!class_exists('WFInstall')) {
+            require_once(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jce' . DS . 'install.php');
+        }
+
+        $installer = method_exists($parent, 'getParent') ? $parent->getParent() : $parent->parent;
+
+        return WFInstall::install($installer);
+    }
+
+    public function uninstall() {
+        if (!class_exists('WFInstall')) {
+            require_once(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_jce' . DS . 'install.php');
+        }
+
+        return WFInstall::uninstall();
+    }
+
+    public function update($parent) {
+        return $this->install($parent);
+    }
+
+    /*function preflight($type, $parent) {
+        $db = JFactory::getDBO();
+        
+        // remove admin menu emtries
+        $db = JFactory::getDBO();
+        $db->setQuery('DELETE FROM #__menu WHERE alias = "jce" AND menutype = "main"');
+        
+        $db = JFactory::getDBO();
+        $db->setQuery('DELETE FROM #__menu WHERE alias = "wf-menu-cpanel" AND menutype = "main"');
+        
+        $db = JFactory::getDBO();
+        $db->setQuery('DELETE FROM #__menu WHERE alias = "wf-menu-config" AND menutype = "main"');
+        
+        $db = JFactory::getDBO();
+        $db->setQuery('DELETE FROM #__menu WHERE alias = "wf-menu-profiles" AND menutype = "main"');
+        
+        $db = JFactory::getDBO();
+        $db->setQuery('DELETE FROM #__menu WHERE alias = "wf-menu-install" AND menutype = "main"');
+        
+        $db->query();
+        $db->setQuery('DELETE FROM #__assets WHERE title = "com_jce"');
+        $db->query();
+    }*/
+}
+
+?>
