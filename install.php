bf2a371f1d7107aa247ead338d285ffa4337c187
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-05-18 13:29:13 +0100

    Fix install version check

--- install.php
+++ install.php
@@ -44,7 +44,7 @@ class WFInstall {
     public static function install($installer) {
         
         // check PHP version
-        if (version_compare(PHP_VERSION, '5.2.10', '<')) {            
+        if (version_compare(PHP_VERSION, '5.2.1', '<')) {            
             $installer->abort('JCE requires PHP 5.2.1 or later. Your server currently uses ' . PHP_VERSION);
             return false;
         }
