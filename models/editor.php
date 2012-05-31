03ab4b33d7af310f97009771129ef9cd6bf5ae4d
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-05-31 19:02:08 +0100

    Change toolbar defaults

--- models/editor.php
+++ models/editor.php
@@ -52,9 +52,9 @@ class WFModelEditor extends JModel {
 
             // Theme and skins
             $theme = array(
-                'toolbar_location' => array('top', 'bottom', 'string'),
-                'toolbar_align' => array('left', 'center', 'string'),
-                'statusbar_location' => array('bottom', 'top', 'string'),
+                'toolbar_location' => array('top', 'top', 'string'),
+                'toolbar_align' => array('left', 'left', 'string'),
+                'statusbar_location' => array('bottom', 'bottom', 'string'),
                 'path' => array(1, 1, 'boolean'),
                 'resizing' => array(1, 0, 'boolean'),
                 'resize_horizontal' => array(1, 1, 'boolean'),
@@ -85,7 +85,7 @@ class WFModelEditor extends JModel {
 
             // Editor Toggle
             $settings['toggle'] = $wf->getParam('editor.toggle', 1, 1);
-            $settings['toggle_label'] = htmlspecialchars($wf->getParam('editor.toggle_label', '[show/hide]', '[show/hide]'));
+            $settings['toggle_label'] = htmlspecialchars($wf->getParam('editor.toggle_label', '[Toggle Editor]', '[Toggle Editor]'));
             $settings['toggle_state'] = $wf->getParam('editor.toggle_state', 1, 1);
         }// end profile
         //Other - user specified
