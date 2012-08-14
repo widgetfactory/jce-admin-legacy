dd80fafea90398c6f69810f0238f04365661f276
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-08-14 16:10:16 +0100

    Remove getAttribute method and use is_file to check for file/string

--- classes/xml.php
+++ classes/xml.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   	JCE
  * @copyright 	Copyright (c) 2009-2012 Ryan Demmer. All rights reserved.
@@ -8,89 +9,67 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('_JEXEC') or die('RESTRICTED');
 
-class WFXMLElement extends SimpleXMLElement
-{
-	
-	/**
-	 * Reads a XML file.
-	 *
-	 * @param string  $data   Full path and file name.
-	 * @param boolean $isFile true to load a file | false to load a string.
-	 *
-	 * @return mixed WFXMLElement on success | false on error.
-	 * @todo This may go in a separate class - error reporting may be improved.
-	 */
-	public static function getXML($data, $isFile = true)
-	{
-		// Disable libxml errors and allow to fetch error information as needed
-		libxml_use_internal_errors(true);
+class WFXMLElement extends SimpleXMLElement {
 
-		if ($isFile) {
-			// Try to load the xml file
-			$xml = simplexml_load_file($data, 'WFXMLElement');
-		}
-		else {
-			// Try to load the xml string
-			$xml = simplexml_load_string($data, 'WFXMLElement');
-		}
+    /**
+     * Reads a XML file.
+     *
+     * @param string  $data   Full path and file name.
+     *
+     * @return mixed WFXMLElement on success | false on error.
+     */
+    public static function getXML($data) {
+        // Disable libxml errors and allow to fetch error information as needed
+        libxml_use_internal_errors(true);
 
-		if (empty($xml)) {
-			// There was an error
-			JError::raiseWarning(100, JText::_('ERROR_XML_LOAD'));
+        if (is_file($data)) {
+            // Try to load the xml file
+            $xml = simplexml_load_file($data, 'WFXMLElement');
+        } else {
+            // Try to load the xml string
+            $xml = simplexml_load_string($data, 'WFXMLElement');
+        }
 
-			if ($isFile) {
-				JError::raiseWarning(100, $data);
-			}
+        if (empty($xml)) {
+            // There was an error
+            JError::raiseWarning(100, JText::_('ERROR_XML_LOAD'));
 
-			foreach (libxml_get_errors() as $error)
-			{
-				JError::raiseWarning(100, 'XML: '.$error->message);
-			}
-		}
+            if ($isFile) {
+                JError::raiseWarning(100, $data);
+            }
 
-		return $xml;
-	}
-	
-	/**
-	 * Get the name of the element.
-	 *
-	 * Warning: don't use getName() as it's broken up to php 5.2.3
-	 *
-	 * @return	string
-	 */
-	public function name()
-	{
-		if (version_compare(phpversion(), '5.2.3', '>')) {
-			return (string) $this->getName();
-		}
+            foreach (libxml_get_errors() as $error) {
+                JError::raiseWarning(100, 'XML: ' . $error->message);
+            }
+        }
 
-		// Workaround php bug number 41867, fixed in 5.2.4
-		return (string) $this->aaa->getName();
-	}
+        return $xml;
+    }
 
-	/**
-	 * Legacy method to get the element data.
-	 *
-	 * @return		string
-	 * @deprecated	1.6 - Feb 5, 2010
-	 */
-	public function data()
-	{
-		return (string) $this;
-	}
+    /**
+     * Get the name of the element.
+     *
+     * Warning: don't use getName() as it's broken up to php 5.2.3
+     *
+     * @return	string
+     */
+    public function name() {
+        if (version_compare(phpversion(), '5.2.3', '>')) {
+            return (string) $this->getName();
+        }
 
-	/**
-	 * Legacy method gets an elements attribute by name.
-	 *
-	 * @param		string
-	 * @return		string
-	 * @deprecated	1.6 - Feb 5, 2010
-	 */
-	public function getAttribute($name)
-	{
-		return (string) $this->attributes()->$name;
-	}
+        // Workaround php bug number 41867, fixed in 5.2.4
+        return (string) $this->aaa->getName();
+    }
+
+    /**
+     * Get the element data.
+     *
+     * @return string
+     */
+    public function data() {
+        return (string) $this;
+    }
 }
