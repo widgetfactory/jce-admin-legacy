479210bcadaa11fb4900efb8eabc39c4d5ba0621
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-05 12:29:48 +0000

    Check for number value in text

--- elements/list.php
+++ elements/list.php
@@ -45,7 +45,7 @@ class WFElementList extends WFElement {
             $text       = (string) $option;
             $disabled   = (string) $option->attributes()->disabled ? true : false;
 
-            $text = preg_match('#[^\d]#', $text) ? WFText::_($text) : $text;
+            $text = is_numeric($text) ? $text : WFText::_($text);
 
             if (is_array($value)) {
                 $key = array_search($val, $value);
