01eeab462d058f59f8067a90a668ceb9e6493ad2
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-01-10 16:55:13 +0000

    Add exception handler, rename class

--- classes/error.php
+++ classes/error.php
@@ -1,8 +1,8 @@
 <?php
 
-abstract class WFErrorHandler {
+abstract class WFError {
 
-    public static function suppressError($errno, $errstr, $errfile, $errline, $errcontext) {
+    public static function errorHandler($errno, $errstr, $errfile, $errline, $errcontext) {
         switch ($errno) {
             default:
                 if (defined('JCE_REQUEST')) {
@@ -16,9 +16,15 @@ abstract class WFErrorHandler {
                 break;
         }
     }
-
+    
+    public static function exceptionHandler($exception) {
+        echo "Uncaught exception: " , $exception->getMessage(), "\n";
+    }
 }
 
 // suppress E_STRICT warnings
-set_error_handler(array('WFErrorHandler', 'suppressError'), E_ALL | E_STRICT);
+set_error_handler(array('WFError', 'errorHandler'), E_ALL | E_STRICT);
+// suppress uncaught exceptions
+set_exception_handler(array('WFError', 'exceptionHandler'));
+
 ?>
