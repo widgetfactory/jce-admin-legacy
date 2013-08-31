7af8f5395aca9f6ed9d73f0d43b73c1563eb5e7f
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-08-31 10:37:18 +0100

    Remove getEditorFonts

--- models/editor.php
+++ models/editor.php
@@ -639,39 +639,13 @@ class WFModelEditor extends WFModelBase {
      * @return string font family list
      * @param string $add Font family to add
      * @param string $remove Font family to remove
+     * 
+     * Deprecated in 2.3.4
      */
     public function getEditorFonts() {
-        $wf = WFEditor::getInstance();
-
-        $add = explode(';', $wf->getParam('editor.theme_advanced_fonts_add', ''));
-        $remove = preg_split('/[;,]+/', $wf->getParam('editor.theme_advanced_fonts_remove', ''));
-
-        // Default font list
-        $fonts = array('Andale Mono=andale mono,times', 'Arial=arial,helvetica,sans-serif', 'Arial Black=arial black,avant garde', 'Book Antiqua=book antiqua,palatino', 'Comic Sans MS=comic sans ms,sans-serif', 'Courier New=courier new,courier', 'Georgia=georgia,palatino', 'Helvetica=helvetica', 'Impact=impact,chicago', 'Symbol=symbol', 'Tahoma=tahoma,arial,helvetica,sans-serif', 'Terminal=terminal,monaco', 'Times New Roman=times new roman,times', 'Trebuchet MS=trebuchet ms,geneva', 'Verdana=verdana,geneva', 'Webdings=webdings', 'Wingdings=wingdings,zapf dingbats');
-
-        if (count($remove)) {
-            foreach ($fonts as $key => $value) {
-                foreach ($remove as $gone) {
-                    if ($gone) {
-                        if (preg_match('/^' . $gone . '=/i', $value)) {
-                            // Remove family
-                            unset($fonts[$key]);
-                        }
-                    }
-                }
-            }
-        }
-        foreach ($add as $new) {
-            // Add new font family
-            if (preg_match('/([^\=]+)(\=)([^\=]+)/', trim($new)) && !in_array($new, $fonts)) {
-                $fonts[] = $new;
-            }
-        }
-        natcasesort($fonts);
-        return implode(';', $fonts);
+        return "";
     }
     
-       
     /**
      * Return the current site template name
      *
