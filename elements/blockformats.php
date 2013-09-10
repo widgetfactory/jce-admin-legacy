eba0b778427d3939cea9df616380fe6fc2569a3b
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-09-10 16:27:38 +0100

    Allow re-ordering of formats while retaining unchecked values in original order

--- elements/blockformats.php
+++ elements/blockformats.php
@@ -16,7 +16,7 @@ defined('JPATH_BASE') or die('RESTRICTED');
  */
 class WFElementBlockformats extends WFElement {
 
-    protected static $formats = array('p' => 'Paragraph','div' => 'Div','h1' => 'Heading1','h2' => 'Heading2','h3' => 'Heading3','h4' => 'Heading4','h5' => 'Heading5','h6' => 'Heading6','address' => 'Address','code' => 'Code','pre' => 'Preformatted','samp' => 'Sample','span' => 'Span','section' => 'Section','article' => 'Article','hgroup' => 'HGroup','aside' => 'Aside','figure' => 'Figure','dt' => 'Definition Term','dd' => 'Definition List');
+    protected static $formats = array('p' => 'Paragraph', 'div' => 'Div', 'h1' => 'Heading1', 'h2' => 'Heading2', 'h3' => 'Heading3', 'h4' => 'Heading4', 'h5' => 'Heading5', 'h6' => 'Heading6', 'address' => 'Address', 'code' => 'Code', 'pre' => 'Preformatted', 'samp' => 'Sample', 'span' => 'Span', 'section' => 'Section', 'article' => 'Article', 'hgroup' => 'HGroup', 'aside' => 'Aside', 'figure' => 'Figure', 'dt' => 'Definition Term', 'dd' => 'Definition List');
 
     /**
      * Element type
@@ -26,37 +26,56 @@ class WFElementBlockformats extends WFElement {
      */
     var $_name = 'Blockformats';
 
+    /**
+     * array_insert function from http://www.php.net/manual/en/function.array-splice.php#56794
+     * @param array $array
+     * @param integer $position
+     * @param array $insert_array
+     */
+    protected static function array_insert(&$array, $position, $insert_array) {
+        $first_array = array_splice($array, 0, $position);
+        $array = array_merge($first_array, $insert_array, $array);
+    }
+
     public function fetchElement($name, $value, &$node, $control_name) {
 
         if (empty($value)) {
-            $data   = array_keys(self::$formats);
-            $value  = array();
+            $data = array_keys(self::$formats);
+            $value = array();
         } else {
-            $value  = is_array($value) ? $value : explode(",", $value);
-            $data   = array_unique(array_merge($value, array_keys(self::$formats)));
+            $value      = is_array($value) ? $value : explode(",", $value);
+            $formats    = array_keys(self::$formats);
+            // get formats "missing" from $value
+            $diff = array_diff($formats, $value);
+            // pass value to data array
+            $data = $value;
+
+            while (list($key, $val) = each($diff)) {
+                self::array_insert($data, $key, array($val));
+            }
         }
-        
-        $output     = array();
-        
-        $output[]   = '<div class="blockformats">';
-        $output[]   = '<ul>';
+
+        $output = array();
+
+        $output[] = '<div class="blockformats">';
+        $output[] = '<ul>';
 
         // create default font structure
-        foreach($data as $format) {
+        foreach ($data as $format) {
             if (empty($value) || in_array($format, $value)) {
-                $output[] = '<li><input type="checkbox" value="' . $format . '" checked="checked" /><span class="blockformat-'. $format .'">' . self::$formats[$format] . '</span></li>';
+                $output[] = '<li><input type="checkbox" value="' . $format . '" checked="checked" /><span class="blockformat-' . $format . '">' . self::$formats[$format] . '</span></li>';
             } else {
-                $output[] = '<li><input type="checkbox" value="' . $format . '" /><span class="blockformat-'. $format .'">' . self::$formats[$format] . '</span></li>';
+                $output[] = '<li><input type="checkbox" value="' . $format . '" /><span class="blockformat-' . $format . '">' . self::$formats[$format] . '</span></li>';
             }
-            
         }
 
-        $output[]   = '</ul>';
-        $output[]   = '<input type="hidden" name="' . $control_name . '[' . $name . ']" value="" />';
-        $output[]   = '</div>';
-        
+        $output[] = '</ul>';
+        $output[] = '<input type="hidden" name="' . $control_name . '[' . $name . ']" value="" />';
+        $output[] = '</div>';
+
         return implode("\n", $output);
     }
+
 }
 
 ?>
