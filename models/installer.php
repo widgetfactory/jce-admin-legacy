257e3016a9c294aec3b9a3878404cadda3129c28
Ryan Demmer <ryandemmer@gmail.com>
2014-02-24 14:40:41 +0000

    Add wfmediaelement as Related Extension

--- models/installer.php
+++ models/installer.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2013 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2014 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -301,7 +301,7 @@ class WFModelInstaller extends WFModel {
         $params = JComponentHelper::getParams('com_jce');
 
         // pre-defined array of other plugins
-        $related = preg_replace('#(\w+)#', "'$1'", $params->get('related_extensions', 'jcemediabox,jceutilities,mediaobject,wfmediabox'));
+        $related = preg_replace('#(\w+)#', "'$1'", $params->get('related_extensions', 'jcemediabox,jceutilities,mediaobject,wfmediabox,wfmediaelement'));
         $query  = $db->getQuery(true);
 
         // Joomla! 2.5
