dbc06f0373db67d3ff858c312fbd5b060c7e9556
Ryan Demmer <ryandemmer@gmail.com>
2014-03-03 12:54:07 +0000

    Set CURLOPT_SSL_VERIFYPEER, CURLOPT_SSL_VERIFYHOST to true and include updated cacert.pem

--- helpers/updates.php
+++ helpers/updates.php
@@ -16,11 +16,15 @@ defined('_JEXEC') or die();
 class UpdatesHelper {
 
     private static function applyCACert(&$ch) {
-        $cacert = JPATH_LIBRARIES . '/joomla/http/transport/cacert.pem';
+        $cacert = dirname(__FILE__) . '/cacert.pem';
 
         if (file_exists($cacert)) {
             @curl_setopt($ch, CURLOPT_CAINFO, $cacert);
+            
+            return true;
         }
+        
+        return false;
     }
 
     /**
@@ -44,7 +48,6 @@ class UpdatesHelper {
             curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
             curl_setopt($ch, CURLOPT_TIMEOUT, 10);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             // The @ sign allows the next line to fail if open_basedir is set or if safe mode is enabled
             @curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             @curl_setopt($ch, CURLOPT_MAXREDIRS, 20);
@@ -229,7 +232,6 @@ class UpdatesHelper {
             curl_setopt($ch, CURLOPT_FAILONERROR, true);
             curl_setopt($ch, CURLOPT_HEADER, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
             curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 
@@ -263,7 +265,6 @@ class UpdatesHelper {
         curl_setopt($ch, CURLOPT_FAILONERROR, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         // Pretend we are IE7, so that webservers play nice with us
